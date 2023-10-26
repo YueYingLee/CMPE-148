0c650bced5f5ca37067111b62d14cb96c0273193
JustinYSJSU <justin.yamamoto@sjsu.edu>
2023-10-25 22:21:22 -0700

    home page style

--- app/user_search.py
+++ app/user_search.py
@@ -3,5 +3,5 @@ from wtforms import StringField, SubmitField, FormField, FieldList
 from wtforms.validators import DataRequired
 
 class UserForm(FlaskForm):
-    names = StringField('Enter username(s) seperate by comma', validators=[DataRequired()])
+    names = StringField('Enter up to 10 username(s) seperate by comma below', validators=[DataRequired()])
     submit = SubmitField('Create conversation')
