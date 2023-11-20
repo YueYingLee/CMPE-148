d0b94c499e87306a6637f02da4af08f8446e09a8
YueYingLee <yueyinglee@gmail.com>
2023-11-19 17:54:27 -0800

    modified the register.py for improvement on data section

--- app/register.py
+++ app/register.py
@@ -3,7 +3,7 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, EqualTo
 
 class registerUser(FlaskForm):
- username = StringField('Username',validators =[DataRequired(), Length (min = 6, max = 15)], render_kw ={"placeholder":"Example: user1@131.com"})
+ username = StringField('Username',validators =[DataRequired(), Length (min = 6, max = 15)], render_kw ={"placeholder":"Example: user148"})
  password = PasswordField('New password', validators = [DataRequired(), Length (min=4, max =10), EqualTo('confirm',message='password does not match')])
  confirm = PasswordField ('Confirm password', validators = [DataRequired(), EqualTo('confirm',message='password does not match')])
  submit = SubmitField ("Register")
